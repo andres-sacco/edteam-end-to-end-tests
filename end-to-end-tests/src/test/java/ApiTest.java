7052964435856dcb77c801cfe95c2f38eda88e8a
asacco <andres.sacco@travelx.io>
2024-07-15 18:04:25 -0300

    final

--- end-to-end-tests/src/test/java/ApiTest.java
+++ end-to-end-tests/src/test/java/ApiTest.java
@@ -1,9 +1,23 @@
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class ApiTest {
 
-    @Karate.Test
+    /*@Karate.Test
     Karate runAllTests() {
         return Karate.run().tags("~@ignore").relativeTo(getClass());
+    }*/
+
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:reservation/")
+                .parallel(12); // Number of threads
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 }
