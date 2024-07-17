9cec903038985dec2755c9e58d4687e6b45c26eb
asacco <andres.sacco@travelx.io>
2024-07-17 18:11:53 -0300

    final

---
+++ end-to-end-tests/src/test/java/reservation/documentation/PlaywrightTest.java
@@ -0,0 +1,26 @@
+package reservation.documentation;
+
+import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.*;
+
+class PlaywrightTest {
+
+    @Test
+    void should_validate_documentation() {
+        Playwright playwright = Playwright.create();
+        BrowserType browserType = playwright.chromium();
+        Browser browser = browserType.launch();
+        Page page = browser.newPage();
+        page.navigate("http://localhost:3070/api/flights/reservation/documentation");
+
+        // Interact with the Swagger UI as needed
+        page.click("text=Explore");
+        page.waitForSelector("text=OpenAPI definition");
+
+        page.click("#operations-Reservation-getReservationById");
+        page.waitForSelector("text=Get the information about one reservation");
+
+        browser.close();
+        playwright.close();
+    }
+}
---
+++ end-to-end-tests/src/test/java/reservation/documentation/get-documentation.feature
@@ -0,0 +1,13 @@
+Feature: Swagger API
+
+  Background:
+    * configure driver = { type: 'playwright-chromium' , showDriverLog: true }
+
+  Scenario: Validate Swagger API
+    Given driver reservationUrl + '/documentation'
+    When waitFor("div.swagger-ui")
+    And waitFor(".operation-tag-content")
+    And click("button.download-url-button.button")
+    And click(".opblock-get")
+    Then match html("p") contains '<p>Operations about the reservation entity</p>'
+
