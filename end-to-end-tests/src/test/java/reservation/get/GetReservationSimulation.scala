9dc1084b3e8a42833f9ed990999a31b6bbc46d56
asacco <andres.sacco@travelx.io>
2024-07-18 12:14:09 -0300

    final

--- end-to-end-tests/src/test/java/reservation/get/GetReservationSimulation.scala
+++ end-to-end-tests/src/test/java/reservation/get/GetReservationSimulation.scala
@@ -23,6 +23,28 @@ class GetReservationSimulation extends Simulation {
 
   setUp(
     //The number of users that receive the endpoint for a period of time
-    get.inject(rampUsers(1) during (1 seconds)).protocols(protocol)
-  ).assertions()
+    get.inject(
+      // Simple
+      rampUsers(10) during (10 seconds) // increment the number of users
+
+      // atOnceUsers(10) // 10 users one time at the same moment
+      // constantUsersPerSec(20).during(15).randomized //20 users random
+      // stressPeakUsers(100).during(10)
+
+      // Multiple
+      // constantConcurrentUsers(10).during(10),
+      // rampConcurrentUsers(10).to(20).during(10)
+    ).protocols(protocol)
+  ).assertions(
+    global.responseTime.max.lt(2000), // Maximum response time should be less than 2 seconds
+    global.responseTime.mean.lt(500), // Mean response time should be less than 500 ms
+
+    global.responseTime.percentile1.lt(1000), // 50th percentile response time should be less than 1 second
+    global.responseTime.percentile2.lt(700), // 75th percentile response time should be less than 700 millisecond
+    global.responseTime.percentile3.lt(500), // 95th percentile response time should be less than 500 millisecond
+    global.responseTime.percentile4.lt(100), // 99th percentile response time should be less than 100 millisecond
+
+    global.successfulRequests.percent.gt(95), // More than 95% of requests should be successful
+    global.failedRequests.count.is(0) // There should be no failed requests
+  )
 }
